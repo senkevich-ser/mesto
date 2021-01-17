06a46086d01418591aaf8d5cad6e9698750cf213
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2021-01-17 08:12:27 +0300

    04.02

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+let profileEditButton = document.querySelector(".lead__pencil");
+let crossClosePopup = document.querySelector(".popup__close");
+let leadElementInitial = document.querySelector(".lead__title");
+let leadElementExplorer = document.querySelector(".lead__subtitle");
+let inputName = document.querySelector(".popup__input-text_type_name");
+let inputExplorer = document.querySelector(".popup__input-text_type_explorer");
+let popup = document.querySelector(".popup");
+let submitButton = document.querySelector(".popup__submit-btn");
+let formElement = document.querySelector(".popup__inputs");
+
+function openPopap() {
+  popup.classList.add("popup_opened");
+}
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+profileEditButton.addEventListener("click", function () {
+  openPopap();
+  inputName.value = leadElementInitial.textContent;
+  inputExplorer.value = leadElementExplorer.textContent;
+});
+
+crossClosePopup.addEventListener("click", function () {
+  closePopup();
+});
+
+submitButton.addEventListener("click", function () {
+  closePopup();
+});
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  leadElementInitial.textContent = inputName.value;
+  leadElementExplorer.textContent = inputExplorer.value;
+}
+formElement.addEventListener("submit", handleFormSubmit);
