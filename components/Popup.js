ed7b72877d829451c630d9d3569055b4859886ab
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2021-06-30 04:53:46 +0300

    add esc close

--- components/Popup.js
+++ components/Popup.js
@@ -1,32 +1,34 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleCloseListener = this._handleEscClose.bind(this);
   }
 
   _handleEscClose(evt) {
     if (evt.key === "Escape") {
-      close.bind(this._popup);
+      this.close();
     }
   }
-
+  _handleCloseClick(evt) {
+    if (
+      evt.target.classList.contains("popup") ||
+      evt.target.classList.contains("popup__close-cross")
+    ) {
+      this.close();
+    }
+  }
   open() {
     this._popup.classList.add("popup_opened");
-    document.addEventListener("keydown", (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.addEventListener("keydown", this._handleCloseListener);
     this.setEventListeners();
   }
   close() {
     this._popup.classList.remove("popup_opened");
-    document.removeEventListener("keydown", (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.removeEventListener("keydown", this._handleCloseListener);
   }
   setEventListeners() {
-    this._popup
-      .querySelector(".popup__close-cross")
-      .addEventListener("click", () => {
-        this.close();
-      });
+    this._popup.addEventListener("click", (evt) => {
+      this._handleCloseClick(evt);
+    });
   }
 }
