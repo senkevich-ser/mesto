f346ee3ebddba021878073cd85bf156fde30b009
senkevich-ser <72697195+senkevich-ser@users.noreply.github.com>
2021-07-19 11:22:04 +0400

    add hover by avatar

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -26,5 +26,5 @@ const initialCards = [
 ];
 const editProfBtn = document.querySelector(".lead__pencil");
 const addCardBtn = document.querySelector(".lead__button");
-const editAvatarBtn = document.querySelector(".lead__image");
+const editAvatarBtn = document.querySelector(".lead__avatarButton");
 export { initialCards,editProfBtn,addCardBtn,editAvatarBtn};
